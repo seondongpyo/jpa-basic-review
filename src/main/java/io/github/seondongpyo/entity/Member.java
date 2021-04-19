b97b4b9bf923173d72f1b788544eb08d6b6f5d10
seondongpyo <psd1390@gmail.com>
2021-04-20 01:09:29 +0900

    feat: add Member entity

---
+++ src/main/java/io/github/seondongpyo/entity/Member.java
@@ -0,0 +1,36 @@
+package io.github.seondongpyo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    public Member() {
+    }
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
