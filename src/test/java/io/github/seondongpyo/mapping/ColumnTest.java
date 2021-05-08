61fbf0da4bc1ea8d57079979c242187e40193b91
seondongpyo <seondongpyo@gmail.com>
2021-05-08 23:38:21 +0900

    test(@Column): updatable = false로 설정하면 해당 엔티티의 필드 값이 변경되어도 DB에선 값이 수정되지 않는다.

--- src/test/java/io/github/seondongpyo/mapping/ColumnTest.java
+++ src/test/java/io/github/seondongpyo/mapping/ColumnTest.java
@@ -37,7 +37,7 @@ public class ColumnTest {
 		Employee employee = new Employee();
 		employee.setAddress("서울특별시 송파구");
 		em.persist(employee);
-		
+
 		em.flush();
 		em.clear();
 
@@ -46,6 +46,29 @@ public class ColumnTest {
 
 		// then
 		assertThat(foundEmployee.getAddress()).isNull();
+	}
+
+	@Test
+	@DisplayName("updatable : false 설정 시 데이터베이스에 값이 수정되지 않는다.")
+	void updatable() {
+		// given
+		Employee employee = new Employee();
+		employee.setName("홍길동");
+		em.persist(employee);
+
+		em.flush();
+		em.clear();
+
+		// when
+		Employee foundEmployee = em.find(Employee.class, employee.getId());
+		foundEmployee.setName("김길동");
+
+		em.flush();
+		em.clear();
+
+		// then
+		String name = em.find(Employee.class, employee.getId()).getName();
+		assertThat(name).isEqualTo("홍길동").isNotEqualTo("김길동");
 	}
 
 }
