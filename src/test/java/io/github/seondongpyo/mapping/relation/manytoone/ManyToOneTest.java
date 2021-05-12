17cfeb9e14944a2a02eadd9e03749585a12d6a94
seondongpyo <psd1390@gmail.com>
2021-05-13 01:52:28 +0900

    test: 다대일 양방향 연관관계 매핑 시 주의 (한 쪽에만 값을 입력할 경우)

--- src/test/java/io/github/seondongpyo/mapping/relation/manytoone/ManyToOneTest.java
+++ src/test/java/io/github/seondongpyo/mapping/relation/manytoone/ManyToOneTest.java
@@ -4,7 +4,6 @@ import io.github.seondongpyo.mapping.relation.manytoone.bidirectional.Professor;
 import io.github.seondongpyo.mapping.relation.manytoone.bidirectional.University;
 import io.github.seondongpyo.mapping.relation.manytoone.unidirectional.School;
 import io.github.seondongpyo.mapping.relation.manytoone.unidirectional.Student;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -78,6 +77,25 @@ public class ManyToOneTest {
         assertThat(foundUniversity.getProfessors()).hasSize(1);
         assertThat(foundUniversity.getProfessors().get(0).getName()).isEqualTo("김길동");
         assertThat(foundProfessor.getUniversity().getName()).isEqualTo("한국대학교");
+    }
+
+    @DisplayName("다대일 양방향 연관관계 매핑 시 주의 - 한 쪽에만 값을 입력할 경우")
+    @Test
+    void setValueBothSide() {
+        // given
+        University university = new University("낙성대");
+        em.persist(university);
+
+        Professor professor = new Professor("이상준");
+        university.getProfessors().add(professor); // university에만 값 세팅
+//        professor.appoint(university); // professor에는 값 세팅 X
+        em.persist(professor);
+
+        // when
+        Professor foundProfessor = em.find(Professor.class, professor.getId()); // 1차 캐시에서 조회
+
+        // then
+        assertThat(foundProfessor.getUniversity()).isNull();
     }
 
 }
